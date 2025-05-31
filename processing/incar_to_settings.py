d5e1ff31b7c9a302947298b6b0160c80903833db
luciancahil <johndoe@example.com>
2025-05-30 17:48:41 -0700

    added processing script

--- processing/incar_to_settings.py
+++ processing/incar_to_settings.py
@@ -1,3 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--kpoint', type=str, default=None, help='K-point coordinates', required=True)
+args = parser.parse_args()
+
+
 input = open("INCAR", mode='r')
 
 output = open("settings.csv", mode='w')
@@ -6,4 +14,6 @@ output = open("settings.csv", mode='w')
 for line in input:
     parts = line.split("=")
 
-    output.write("{},{}\n".format(parts[0].strip(), parts[1].strip()))
+    output.write("{},{}\n".format(parts[0].strip().lower(), parts[1].strip()))
+
+output.write("kpoints,{}\n".format(args.kpoint))
---
+++ processing/process_incar.sh
@@ -0,0 +1,5 @@
+kpoints="7"
+
+echo incar_to_settings.py --kpoint $kpoints
+
+python incar_to_settings.py --kpoint $kpoints
